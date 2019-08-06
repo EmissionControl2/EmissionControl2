55cd946ee9711ddfba174275e98c2db3f78ab2f9
Jack Kilgore <kilgorej99@gmail.com>
2019-08-06 11:35:10 -0700

    playing around with look-up tables and interpolation

--- dsp/alloTests/test.cpp
+++ dsp/alloTests/test.cpp
@@ -1,5 +1,10 @@
 #include "al/core.hpp"
 #include "Gamma/Oscillator.h"
+#include <math.h>
+
+#define SAMPLE_RATE (48000);
+#define BLOCK_SIZE (512);
+
 
 using namespace al;
 
@@ -7,7 +12,9 @@ class grain {
 public:
 
 private:
-
+  float duration;
+  float amplitude;
+  float space;
 };
 
 class grainContainer{
@@ -16,13 +23,84 @@ public:
 private: 
 };
 
-struct sine_app : App
+
+
+class testPhasor{
+public:
+  float operator()() {
+    phase += increment;
+    if (phase < 0) phase += 1;
+    if (phase > 1) phase -= 1;
+    return phase;
+  }
+  
+  void frequency(float hertz) {
+    increment = hertz / SAMPLE_RATE;
+  }
+private: 
+  float phase = 0.0; 
+  float increment = 0.0001;
+};
+
+class testSine : public testPhasor {
+public:
+  testSine() {
+    for(int i = 0; i < SIZE; i++) 
+      table[i] = std::sin(2 * M_PI * i / SIZE); // this will give one period of a sine wave ! !
+  }
+
+  float operator()() {
+    float phase = testPhasor::operator()();
+    return table[int(SIZE * phase)];
+  }
+
+private: 
+  static const int SIZE = 2048;
+  float table[SIZE];
+};
+
+class linearInterpSine : public testPhasor {
+public:
+  linearInterpSine() {
+    table = new float[SIZE];
+    for (int i = 0; i < SIZE; i++)
+      table[i] = std::sin(2 * M_PI * i / SIZE);
+  }
+  float operator()() {
+    float phase = testPhasor::operator()() * SIZE;
+    int left = int(phase);
+    int right = left + 1; 
+    float m = table[right] - table[left]; //slope      
+    return ( m * (phase - left) + table[left]);
+  }
+private:
+  int counter = 0;
+  static const size_t SIZE = 2048;
+  float *table; 
+};
+
+class asrEnvelope {
+
+};
+
+struct test_app : App
 {
-  gam::Sine<> osc;
+  // testSine osc;
+  // testSine osc1;
+  // testSine osc2;
+  // testSine osc3;
+  linearInterpSine osc;
+  linearInterpSine osc1;
+  linearInterpSine osc2;
+  linearInterpSine osc3;
+
 
   void onCreate() override {
-    gam::Domain::master().spu(audioIO().framesPerSecond());
-    osc.freq(80);
+    osc.frequency(40);
+    osc1.frequency(60);
+    osc2.frequency(120);
+    osc3.frequency(9000);
+
   }
 
   void onDraw(Graphics& g) override {
@@ -31,16 +109,20 @@ struct sine_app : App
 
   void onSound(AudioIOData& io) override {
     while (io()) {
-      float s = osc(); // Generate next sine wave sample
-      s *= 0.2; // Scale the sample down a bit for output
-      io.out(0) = s;
-      io.out(1) = s;
+      float out;
+      float s = osc();
+      float s1 = osc1();
+      float s2 = osc2();
+      float s3 = osc3();
+      out = ((s * 0.4) + (s1 * 0.35) + (s2*0.15) + (s3*0.002)) * 0.5;
+      io.out(0) = out;
+      io.out(1) = out;
     }
   }
 };
 int main()
 {
-  sine_app app;
+  test_app app;
   app.initAudio(48000, 512, 2, 0);
   app.start();
 }
