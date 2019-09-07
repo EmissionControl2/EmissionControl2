d0f2396cc758f282a4170ffabf281cf76781ddbf
Jack Kilgore <kilgorej99@gmail.com>
2019-09-06 18:40:46 -0700

    naive implemntation of exponential decay/attack; (is this all for not; HOW do i interpolate between these?; should i be using a table? :(

--- dsp/include/const.h
+++ dsp/include/const.h
@@ -1,12 +1,16 @@
 #ifndef CONST_H 
 #define CONST_H
 
+namespace consts {
+
 const int SAMPLE_RATE = 44100;
 const int BLOCK_SIZE = 128;
 const int AUDIO_OUTS = 2;
 const int DEVICE_NUM = -1;
 enum streamType {synchronous, asynchronous, sequenced, derived};
 enum waveform {SINE, SQUARE, SAW, NOISE};
+
+}
 
 
 #endif
--- dsp/src/main.cpp
+++ dsp/src/main.cpp
@@ -5,7 +5,7 @@ int main(){    // Create app instance
   ecInterface app; //why does this make extraneous calls? Synth GUI manager causes it
   std::cout << "HEREEEEmade it\n";
   // Set up audio
-  app.initAudio(SAMPLE_RATE, BLOCK_SIZE, AUDIO_OUTS, DEVICE_NUM);
+  app.initAudio(consts::SAMPLE_RATE, consts::BLOCK_SIZE, consts::AUDIO_OUTS, consts::DEVICE_NUM);
   // Set sampling rate for Gamma objects from app's audio
   gam::sampleRate(app.audioIO().framesPerSecond());
   app.dimensions(1200, 800);
