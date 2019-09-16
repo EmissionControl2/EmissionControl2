4506b17f06a91cf8d9ecc948bdcbff3b75be6f72
Jack Kilgore <kilgorej99@gmail.com>
2019-09-16 12:26:32 -0700

    Commenting lots of code

--- dsp/include/utility.h
+++ dsp/include/utility.h
@@ -49,25 +49,30 @@ private:
 };
 
 
-//envelope generator for creating exponetial decay and exponential growth.
+/**
+ * Envelope generator for creating exponetial decay and exponential growth.
+ */
 class expo {
 public:
+
+  /**
+   * Used to reset values to original position.
+   */
   void set() {
     if (mTotalS <= 0) mTotalS = 1;
     if (mThresholdY <= 0) mThresholdY = 0.001, mThresholdX = -1 * std::log(0.001);
     mX = 0;
     mY = mThresholdY;
-    mIncrementX = (mThresholdX / mTotalS); // -ln(0.001) 
-  }                                         // where 0.001 is our threshold for when we release envelope
-  void set(float seconds, bool reverse,float threshold, float alpha) {
-    mTotalS = seconds * consts::SAMPLE_RATE;
-    mReverse = reverse;
-    mThresholdY = threshold;
-    mThresholdX = -1 * std::log(threshold);
-    mAlpha = alpha;
-    set();
-  }
+    mIncrementX = (mThresholdX / mTotalS); 
+  } 
 
+  /**
+   * Set parameters of envelope. 
+   * 
+   * param[in] The duration of the envelope in seconds. 
+   * param[in] If true, the envelope is reversed and becomes an exponential growth envelope. 
+   * param[in] Sets the amplitude threshold for when to release the envelope.  
+   */                                        
   void set(float seconds, bool reverse, float threshold) {
     mTotalS = seconds * consts::SAMPLE_RATE;
     mReverse = reverse;
@@ -87,12 +92,18 @@ public:
     set();
   }
 
+  /**
+   * Returns true if the envelope is finished. False otherwise.
+   */
   bool done() { 
     return mX == 0;
   }
 
   void increment() {mX += mIncrementX;}
 
+  /**
+   * Envelope generated at the audio rate. 
+   */
   float operator()() { 
   if(!mReverse) {
     if(mX < mThresholdX * 0.01) { // ratio of initial ramp up to 1
@@ -100,7 +111,7 @@ public:
       mX += mIncrementX;
     }
     else if(mX < mThresholdX) {
-        mY = powf(M_E, -1 * mX * mAlpha + (mThresholdX * 0.01) ); // this compensates for initial ramp up 
+        mY = powf(M_E, -1 * mX + (mThresholdX * 0.01) ); // this compensates for initial ramp up 
         mX += mIncrementX;
     } else  {
       mY = mThresholdY; 
@@ -123,19 +134,38 @@ public:
   }
   return mY;
   }
+
 private:
-  float mAlpha = 1, mIncrementX, mX= 0, mY = 0.001, mThresholdX = -1 * std::log(0.001), mThresholdY = 0.001;
+  float mIncrementX, mX= 0, mY = 0.001, mThresholdX = -1 * std::log(0.001), mThresholdY = 0.001;
   bool mReverse = 0;
   int mTotalS = 1;
   int tempCounter = 0;
 };
 
-// envelope GENERATOR for creating a tukey window
+/**
+ * Envelope generator for creating a tukey envelope. 
+ * A tukey window is like a fatter Hann envelope.
+ */
 class tukey {
 public: 
+
+  /**
+   * Used to reset values to original position.
+   */
   void set() {
     if (totalS <= 0) totalS = 1 ;
+    currentS = 0;
+    value = 0;
   }
+
+  /**
+   * Set parameters of envelope. 
+   * 
+   * param[in] The duration of the envelope in seconds. 
+   * param[in] FROM 0 to 1. A coefficient that determines the ratio of the sustain to the attack and release. 
+   *  0 being a rectangular envelope and 1 being a Hann envelope.
+   *  https://www.mathworks.com/help/signal/ref/tukeywin.html
+   */
   void set(float seconds,float alpha) {
     this->alpha = alpha;
     totalS = seconds * consts::SAMPLE_RATE;
@@ -146,10 +176,16 @@ public:
     set();
   }
 
+  /**
+   * Return true if envelope is finished. False otherwise.
+   */
   bool done() { return totalS == currentS; }
 
   void increment() {currentS++;}
 
+  /**
+   * Envelope generated at the audio rate.
+   */
   float operator()() {
     if(currentS < (alpha * totalS)/2) {
         value = 0.5 * (1 + std::cos(M_PI * (2 * currentS / (alpha * totalS) - 1)));
@@ -168,7 +204,9 @@ private:
   int currentS = 0, totalS = 1;
 };
 
-
+/**
+ * A Buffer struct that has multi-sound file loading functionalities.
+ */ 
 template<typename T>
 struct Buffer {
   T* data;
@@ -225,6 +263,12 @@ struct Buffer {
   }
 };
 
+/**
+ * Load soundfile into a buffer in memory. 
+ * 
+ * param[in] The filename. An absolute filename is preferred. 
+ * param[out] A vector holding the audio buffers. 
+ */
 void load(std::string fileName, std::vector<Buffer<float>*>& buf) { //only works on mono files for now
   al::SearchPaths searchPaths;
   // searchPaths.addSearchPath("../../samples");
