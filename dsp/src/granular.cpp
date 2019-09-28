a9ef34f36e06b7cf59d3d415626e62d590d85811
Jack Kilgore <kilgorej99@gmail.com>
2019-09-28 12:13:46 -0700

    added LFO drop down menu to all paramters and changed up GUI a bit

--- dsp/src/granular.cpp
+++ dsp/src/granular.cpp
@@ -34,24 +34,33 @@ public:
 
   voiceScheduler grainScheduler{consts::SAMPLE_RATE};
   ecParameter grainRate {"grainRate", "", 1, "", 0.5, 100, consts::SINE, 0};
+  ParameterMenu grainRateLFO {""}; 
   ecParameter modGrainRateWidth {"modGrainRateWidth", "", 0, "", 0, 1};
   ecParameter asynchronicity {"asynchronicity", "", 0.0, "", 0.0, 1.0, consts::SINE};
+  ParameterMenu asyncLFO {" "}; // Using spaces so the ParameterGUI::drawMenu behaves correctly/
   ecParameter modAsynchronicityWidth {"modAsynchronicityWidth", "", 0, "", 0, 1};
   ecParameter intermittency {"intermittency", "", 0,"", 0, 1};
+  ParameterMenu intermittencyLFO {"  "}; 
   ecParameter modIntermittencyWidth {"modIntermittencyWidth", "", 0, "", 0, 1};
   ecParameterInt streams {"streams", "", 1,"", 1, 12};
+  ParameterMenu streamsLFO {"   "}; 
   ecParameter modStreamsWidth {"modStreamsWidth", "", 0, "", 0, 1};
 
   ecParameter grainDurationMs {"grainDurationMs", "", 25, "", 0.01, 1000};
+  ParameterMenu grainDurationLFO {"    "}; 
   ecParameter modGrainDurationWidth {"modGrainDurationWidth", "", 0, "", 0, 1};
   ecParameter envelope {"envelope", "", 0.5, "", 0, 1};
+  ParameterMenu envelopeLFO {"     "}; 
   ecParameter modEnvelopeWidth {"modEnvelopeWidth", "", 0, "", 0, 1};
   ecParameter tapeHead{"tapeHead", "", 0, "", 0, 1};
+  ParameterMenu tapeHeadLFO {"      "}; 
   ecParameter modTapeHeadWidth {"modTapeHeadWidth", "", 0, "", 0, 1};
-  ecParameter playbackRate {"playbackRate", "", 1, "", -2, 2};
+  ecParameter playbackRate {"playbackRate", "       ", 1, "", -2, 2};
+  ParameterMenu playbackRateLFO {"        "}; 
   ecParameter modPlaybackRateWidth {"modPlaybackRateWidth", "", 0, "", 0, 1};
 
-  ecParameter volumedB {"volumedB", "", -6, "", -60, 6};
+  ecParameter volumeDB {"volumeDB", "", -6, "", -60, 6};
+  ParameterMenu volumeLFO {"         "}; 
   ecParameter modVolumeWidth {"modVolumeWidth", "", 0, "", 0, 1};
   
 
@@ -59,7 +68,7 @@ public:
   ecParameter modSineFrequency {"modSineFrequency", "",1, "", 0.01, 40};
   ecParameter modSinePhase {"modSinePhase", "", 0, "", 0, 1};
   ecParameter modSquareFrequency {"modSquareFrequency", "",1,"", 0.01, 40};
-  ecParameter modSquareWidth {"modSquareWidth", "",1,"", 0, 1};
+  ecParameter modSquareWidth {"modSquareWidth", "",0.5,"", 0, 1};
   ecParameter modSawFrequency {"modSawFrequency", "",1,"", 0.01, 40};
   ecParameter modSawWidth {"modSawWidth", "",1,"", 0, 1};
   
@@ -80,12 +89,44 @@ public:
 
   virtual void init() override {
 
-
-    grainScheduler.configure(grainRate, 0.0, 0.0);
-
-    streams.registerChangeCallback([&](int value) {
-      grainScheduler.setPolyStream(consts::synchronous, value);
+    //MUST USE THIS ORDER
+    grainRateLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    grainRateLFO.registerChangeCallback([&](int value) {
+      grainRate.setWaveformIndex(value);
     });
+    asyncLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    asyncLFO.registerChangeCallback([&](int value) {
+      asynchronicity.setWaveformIndex(value);
+    });
+    intermittencyLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    intermittencyLFO.registerChangeCallback([&](int value) {
+      intermittency.setWaveformIndex(value);
+    });
+    streamsLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    streamsLFO.registerChangeCallback([&](int value) {
+      streams.setWaveformIndex(value);
+    });
+    grainDurationLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    grainDurationLFO.registerChangeCallback([&](int value) {
+      grainDurationMs.setWaveformIndex(value);
+    });
+    envelopeLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    envelopeLFO.registerChangeCallback([&](int value) {
+      envelope.setWaveformIndex(value);
+    });
+    tapeHeadLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    tapeHeadLFO.registerChangeCallback([&](int value) {
+      tapeHead.setWaveformIndex(value);
+    });
+    playbackRateLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    playbackRateLFO.registerChangeCallback([&](int value) {
+      playbackRate.setWaveformIndex(value);
+    });
+    volumeLFO.setElements({"Sine", "Square", "Saw", "Noise"});
+    volumeLFO.registerChangeCallback([&](int value) {
+      volumeDB.setWaveformIndex(value);
+    });
+    grainScheduler.configure(grainRate, 0.0, 0.0);
 
     modSineFrequency.registerChangeCallback([&](float value) {
       modSine.setFrequency(value);
@@ -124,7 +165,7 @@ public:
 
       // THIS IS WHERE WE WILL MODULATE THE GRAIN SCHEDULER
 
-
+      // NOTE grainRate noise isnt very perceptible 
       if(modGrainRateWidth.get() > 0)  // modulate the grain rate
         grainScheduler.setFrequency(grainRate.getModParam(modSineValue, modSquareValue, modSawValue, modNoiseValue, 
         modGrainRateWidth.get())); 
@@ -184,7 +225,7 @@ public:
     grainSynth.render(io);
 
     io.frame(0); 
-    float amp = powf(10,volumedB.get()/20);
+    float amp = powf(10,volumeDB.get()/20);
     while (io()) {
       io.out(0) *=  amp ; // this manipulates the entire stream on the channel level 
       io.out(1) *=  amp ; //* mEnv() 
