e4e9dfc84cf85dcca71a0a3ebb6fb73378e1f573
Jack Kilgore <kilgorej99@gmail.com>
2019-12-20 13:10:43 -0800

    libsndfile dylib is now portable; just need to fix the dependencies of libsndfile

---
+++ dsp/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# if needed to create debug or other builds replace 'release' in the following
+(
+ rm -r -f bin/ 
+ rm -r -f build/
+)
+
--- dsp/configure.sh
+++ dsp/configure.sh
@@ -2,10 +2,16 @@
 
 # if needed to create debug or other builds replace 'release' in the following
 (
+  mkdir -p bin 
+  cd bin 
+  cp -r ../../samples .
+  mkdir -p soundOutput
+  cp -r ../external/libraries .
+	cd ..
   mkdir -p build
   cd build
   mkdir -p release
   cd release
-  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
 )
 
