c53bb66d93a5e099b534e7c7521c1c4f92e92b01
Jack Kilgore <kilgorej99@gmail.com>
2019-12-20 13:10:43 -0800

    libsndfile dylib is now portable; just need to fix the dependencies of libsndfile

--- dsp/run.sh
+++ dsp/run.sh
@@ -2,11 +2,13 @@
 (
   # utilizing cmake's parallel build options
   # recommended: -j <number of processor cores + 1>
-  cmake --build ./build/release -j 5
+  cmake --build ./build/release -j 5 
+  install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @rpath/libsndfile/libsndfile.1.dylib bin/emissionControl
+  install_name_tool -change usr/local/opt/flac/lib/libFLAC.8.dylib @rpath/libsndfile/libFLAC.8.dylib bin/emissionControl
 )
 
 result=$?
 if [ ${result} == 0 ]; then
    cd bin
-  ./emissionControl
+  ./emissionControl -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 
 fi
