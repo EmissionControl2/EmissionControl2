e85278cf77805d544454b0fc8c217f3892bb1d23
Rodney DuPlessis <rodney.dup@hotmail.com>
2020-04-14 13:31:02 -0700

    WIP crashing a lot

--- dsp/include/ecSynth.h
+++ dsp/include/ecSynth.h
@@ -16,192 +16,191 @@
 #include "al/ui/al_Parameter.hpp"
 
 /**** CSTD LIB ****/
-#include <memory>
 #include <math.h> 
+#include <memory>
 
 class ecSynth : public al::SynthVoice {
-public:
-  /**
-   * The number of modulators in ecSynth.
-   */
-  const static int NUM_MODULATORS = 4;
+   public:
+    /**
+     * Ringbuffers for oscilloscope
+     */
+    int oscBufferSize = consts::SAMPLE_RATE * 10;
+    util::RingBuffer oscBufferL{oscBufferSize};
+    util::RingBuffer oscBufferR{oscBufferSize};
 
-  // array of lfo parameters to draw later
-  std::vector<LFOstruct *> LFOparameters;
+    /**
+     * The number of modulators in ecSynth.
+     */
+    const static int NUM_MODULATORS = 4;
 
-  /**
-   * PUBLIC PARAMETERS OF SYNTH
-   */
-  ecParameter grainRate{"Grain_Rate", "", 1,   "",           0.1,
-                        100,          0,  500, consts::SINE, 0};
-  al::ParameterMenu grainRateLFO{"##grainRateLFO"};
-  ecParameter modGrainRateDepth{"modGrainRateDepth", "", 0, "", 0, 1, 0, 1};
+    // array of lfo parameters to draw later
+    std::vector<LFOstruct *> LFOparameters;
 
-  ecParameter asynchronicity{"Asynchronicity", "", 0.0, "", 0.0, 1.0, 0, 1,
-                             consts::SINE};
-  al::ParameterMenu asyncLFO{"##asyncLFO"};
-  ecParameter modAsynchronicityDepth{
-      "modAsynchronicityDepth", "", 0, "", 0, 1, 0, 1};
+    /**
+     * PUBLIC PARAMETERS OF SYNTH
+     */
+    ecParameter grainRate{"Grain_Rate", "", 1, "", 0.1, 100, 0, 500, consts::SINE, 0};
+    al::ParameterMenu grainRateLFO{"##grainRateLFO"};
+    ecParameter modGrainRateDepth{"modGrainRateDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter intermittency{"Intermittency", "", 0, "", 0, 1, 0, 1};
-  al::ParameterMenu intermittencyLFO{"##intermittencyLFO"};
-  ecParameter modIntermittencyDepth{
-      "modIntermittencyDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter asynchronicity{"Asynchronicity", "", 0.0, "", 0.0, 1.0, 0, 1, consts::SINE};
+    al::ParameterMenu asyncLFO{"##asyncLFO"};
+    ecParameter modAsynchronicityDepth{"modAsynchronicityDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameterInt streams{"Streams", "", 1, "", 1, 12, 1, 20};
-  al::ParameterMenu streamsLFO{"##streamsLFO"};
-  ecParameter modStreamsDepth{"modStreamsDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter intermittency{"Intermittency", "", 0, "", 0, 1, 0, 1};
+    al::ParameterMenu intermittencyLFO{"##intermittencyLFO"};
+    ecParameter modIntermittencyDepth{"modIntermittencyDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter grainDurationMs{"Grain_Dur_Ms", "",   30,        "",
-                              0.01,           1000, 0.0000001, 10000};
-  al::ParameterMenu grainDurationLFO{"##grainDurationLFO"};
-  ecParameter modGrainDurationDepth{
-      "modGrainDurationDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameterInt streams{"Streams", "", 1, "", 1, 12, 1, 20};
+    al::ParameterMenu streamsLFO{"##streamsLFO"};
+    ecParameter modStreamsDepth{"modStreamsDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter envelope{"Envelope", "", 0.5, "", 0, 1, 0, 1};
-  al::ParameterMenu envelopeLFO{"##envelopeLFO"};
-  ecParameter modEnvelopeDepth{"modEnvelopeDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter grainDurationMs{"Grain_Dur_Ms", "", 30, "", 0.01, 1000, 0.0000001, 10000};
+    al::ParameterMenu grainDurationLFO{"##grainDurationLFO"};
+    ecParameter modGrainDurationDepth{"modGrainDurationDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter tapeHead{"Tape_Head", "", 0.5, "", 0, 1, 0, 1};
-  al::ParameterMenu tapeHeadLFO{"##tapeHeadLFO"};
-  ecParameter modTapeHeadDepth{"modTapeHeadDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter envelope{"Envelope", "", 0.5, "", 0, 1, 0, 1};
+    al::ParameterMenu envelopeLFO{"##envelopeLFO"};
+    ecParameter modEnvelopeDepth{"modEnvelopeDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter transposition{"Transposition", "", 1, "", -2, 2, -20, 20};
-  al::ParameterMenu transpositionLFO{"##transpositionLFO"};
-  ecParameter modTranspositionDepth{
-      "modTranspositionDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter tapeHead{"Tape_Head", "", 0.5, "", 0, 1, 0, 1};
+    al::ParameterMenu tapeHeadLFO{"##tapeHeadLFO"};
+    ecParameter modTapeHeadDepth{"modTapeHeadDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter filter{"Filter", "", 440, "", 60, 5000, 20, 24000};
-  al::ParameterMenu filterLFO{"##filterLFO"};
-  ecParameter modFilterDepth{"modFilterDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter transposition{"Transposition", "", 1, "", -2, 2, -20, 20};
+    al::ParameterMenu transpositionLFO{"##transpositionLFO"};
+    ecParameter modTranspositionDepth{"modTranspositionDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter resonance{"Resonance", "", 0, "", 0, 1, 0, 1};
-  al::ParameterMenu resonanceLFO{"##resonanceLFO"};
-  ecParameter modResonanceDepth{"modResonanceDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter filter{"Filter", "", 440, "", 60, 5000, 20, 24000};
+    al::ParameterMenu filterLFO{"##filterLFO"};
+    ecParameter modFilterDepth{"modFilterDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter volumeDB{"Volume_DB", "", -6, "", -60, 6, -180, 24};
-  al::ParameterMenu volumeLFO{"##volumeLFO"};
-  ecParameter modVolumeDepth{"modVolumeDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter resonance{"Resonance", "", 0, "", 0, 1, 0, 1};
+    al::ParameterMenu resonanceLFO{"##resonanceLFO"};
+    ecParameter modResonanceDepth{"modResonanceDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameter pan{"Pan", "", 0, "", -1, 1, -1, 1};
-  al::ParameterMenu panLFO{"##panLFO"};
-  ecParameter modPanDepth{"modPanDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter volumeDB{"Volume_DB", "", -6, "", -60, 6, -180, 24};
+    al::ParameterMenu volumeLFO{"##volumeLFO"};
+    ecParameter modVolumeDepth{"modVolumeDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecParameterInt soundFile{"Sound_File", "", 1, "", 1, mClipNum, 1, mClipNum};
-  al::ParameterMenu soundFileLFO{"##soundFileLFO"};
-  ecParameter modSoundFileDepth{"modSoundFileDepth", "", 0, "", 0, 1, 0, 1};
+    ecParameter pan{"Pan", "", 0, "", -1, 1, -1, 1};
+    al::ParameterMenu panLFO{"##panLFO"};
+    ecParameter modPanDepth{"modPanDepth", "", 0, "", 0, 1, 0, 1};
 
-  ecSynth() {}
+    ecParameterInt soundFile{"Sound_File", "", 1, "", 1, mClipNum, 1, mClipNum};
+    al::ParameterMenu soundFileLFO{"##soundFileLFO"};
+    ecParameter modSoundFileDepth{"modSoundFileDepth", "", 0, "", 0, 1, 0, 1};
 
-  /**
-   * @brief Initilialize the synth.
-   */
-  virtual void init(al::AudioIOData *io);
+    ecSynth() {}
 
-  /**
-   * @brief Set data needed to perform granulation correctly.
-   */
-  void setIO(al::AudioIOData *io);
+    /**
+     * @brief Initilialize the synth.
+     */
+    virtual void init(al::AudioIOData *io);
 
-  /**
-   * @brief Audio rate processing of synth.
-   */
-  virtual void onProcess(al::AudioIOData &io) override;
+    /**
+     * @brief Set data needed to perform granulation correctly.
+     */
+    void setIO(al::AudioIOData *io);
 
-  /**
-   * @brief Runs every time a voice is triggered.
-   */
-  virtual void onTriggerOn() override;
+    /**
+     * @brief Audio rate processing of synth.
+     */
+    virtual void onProcess(al::AudioIOData &io) override;
 
-  /**
-   * @brief Runs every time a voice is marked as inactive.
-   */
-  virtual void onTriggerOff() override;
+    /**
+     * @brief Runs every time a voice is triggered.
+     */
+    virtual void onTriggerOn() override;
 
-  /**
-   * @brief Load sound file into memory.
-   *
-   * @param[in] The the filepath to the audio file.
-   */
-  void loadSoundFile(std::string fileName);
+    /**
+     * @brief Runs every time a voice is marked as inactive.
+     */
+    virtual void onTriggerOff() override;
 
-  /**
-   * @brief Load sound files from designated sample folder.
-   *
-   * @return Return the directory path where the files were loaded in from.
-   */
-  std::string loadInitSoundFiles(std::string directory);
+    /**
+     * @brief Load sound file into memory.
+     *
+     * @param[in] The the filepath to the audio file.
+     */
+    void loadSoundFile(std::string fileName);
 
-  /**
-   * @brief Resample all files in SoundClip to match globalSamplingRate.
-   */
-  void resampleSoundFiles();
+    /**
+     * @brief Load sound files from designated sample folder.
+     *
+     * @return Return the directory path where the files were loaded in from.
+     */
+    std::string loadInitSoundFiles(std::string directory);
 
-  /**
-   * @brief Clear sound files stored in memory.
-   *
-   */
-  void clearSoundFiles();
+    /**
+     * @brief Resample all files in SoundClip to match globalSamplingRate.
+     */
+    void resampleSoundFiles();
 
-  /**
-   * @brief Print out what the synth is doing at runtime.
-   *        Used for debugging purposes.
-   *
-   * @param[in] Verbose toggled on if true, toggled off otherwise.
-   */
-  void verbose(bool toggle) { grainSynth.verbose(toggle); }
+    /**
+     * @brief Clear sound files stored in memory.
+     *
+     */
+    void clearSoundFiles();
 
-  /**
-   * @brief Return the number of active voices.
-   *        ie, The number of voices running in the audio callback loop.
-   *
-   * @return The number of active voices.
-   */
-  int getActiveVoices() { return mActiveVoices; }
+    /**
+     * @brief Print out what the synth is doing at runtime.
+     *        Used for debugging purposes.
+     *
+     * @param[in] Verbose toggled on if true, toggled off otherwise.
+     */
+    void verbose(bool toggle) { grainSynth.verbose(toggle); }
 
-  /** WIP -- see cpp file
-   * @brief Nonlinear cubic distortion used for soft clipping.
-   *        Applied to the left and right channels.
-   *        !Run in the process block!
-   */
-  void softClip(al::AudioIOData &io);
+    /**
+     * @brief Return the number of active voices.
+     *        ie, The number of voices running in the audio callback loop.
+     *
+     * @return The number of active voices.
+     */
+    int getActiveVoices() { return mActiveVoices; }
 
-  /**
-   * @brief Simple hardclipping to prevent distorting the DAC.
-   *        Applied to the left and right channels.
-   *        !Run in the process block!
-   */
-  void hardClip(al::AudioIOData &io);
+    /** WIP -- see cpp file
+     * @brief Nonlinear cubic distortion used for soft clipping.
+     *        Applied to the left and right channels.
+     *        !Run in the process block!
+     */
+    void softClip(al::AudioIOData &io);
 
-  /**
-   * WORK IN PROGRESS
-   * @brief Throttles the amount of grains that can be active at once. TODO
-   *
-   * @param[in] The amount of time to the throttle the synth in seconds.
-   * @param[in] The ratio in which to throttle the synth. A number from 0 to 1.
-   */
-  void throttle(float time, float ratio);
+    /**
+     * @brief Simple hardclipping to prevent distorting the DAC.
+     *        Applied to the left and right channels.
+     *        !Run in the process block!
+     */
+    void hardClip(al::AudioIOData &io);
 
-private:
-  float mGlobalSamplingRate, mPrevSR;
+    /**
+     * WORK IN PROGRESS
+     * @brief Throttles the amount of grains that can be active at once. TODO
+     *
+     * @param[in] The amount of time to the throttle the synth in seconds.
+     * @param[in] The ratio in which to throttle the synth. A number from 0 to 1.
+     */
+    void throttle(float time, float ratio);
 
-  al::PolySynth grainSynth{};    /* Polyhpony and interface to audio
-                                               callback */
-  voiceScheduler grainScheduler; /* Schedule grains */
-  std::vector<std::shared_ptr<util::buffer<float>>>
-      soundClip;    /* Store audio buffers in memory */
-  int mClipNum = 0; /* Number of sound files being stored in memory */
-  int mModClip;
+   private:
+    float mGlobalSamplingRate, mPrevSR;
 
-  int controlRateCounter = 0;
-  int mActiveVoices = 0;
-  int mCounter = 0;
-  float mAvgActiveVoices = 0;
-  float mPeakCPU;
-  float mAvgCPU;
-  int *mPActiveVoices = nullptr;
+    al::PolySynth grainSynth{};                                  /* Polyhpony and interface to audio
+                                                                               callback */
+    voiceScheduler grainScheduler;                               /* Schedule grains */
+    std::vector<std::shared_ptr<util::buffer<float>>> soundClip; /* Store audio buffers in memory */
+    int mClipNum = 0; /* Number of sound files being stored in memory */
+    int mModClip;
 
-  std::vector<std::shared_ptr<ecModulator>> Modulators;
+    int controlRateCounter = 0;
+    int mActiveVoices = 0;
+    int mCounter = 0;
+    float mAvgActiveVoices = 0;
+    float mPeakCPU;
+    float mAvgCPU;
+    int *mPActiveVoices = nullptr;
+
+    std::vector<std::shared_ptr<ecModulator>> Modulators;
 };
 
 #endif
