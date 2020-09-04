08ccfe7b25486c2f5a2677f1db0c2201772f7894
Jack Kilgore <kilgorej99@gmail.com>
2020-09-04 13:51:25 -0700

    jk - clean up main file

--- dsp/src/main.cpp
+++ dsp/src/main.cpp
@@ -1,18 +1,7 @@
 #include "ecInterface.h"
 
-int main() { // Create app instance
-
+int main() {
   ecInterface app;
-  // Set up audio
-  app.configureAudio(consts::SAMPLE_RATE, consts::BLOCK_SIZE,
-                     consts::AUDIO_OUTS, consts::DEVICE_NUM);
-  // Set sampling rate for Gamma objects from app's audio
-  gam::sampleRate(app.audioIO().framesPerSecond());
-  app.dimensions(1920, 1080);
-  app.title("Emission Control 2");
-
-  app.audioIO().print();
-
   app.start();
   return 0;
 }
