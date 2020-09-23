3aaafe817b73fd943bfc1e16c142c1d81cf12e75
Rodney DuPlessis <rodney@rodneyduplessis.com>
2020-09-23 16:38:51 -0700

    fixed config and gitignore stuff related to submodules

--- ecSource/scripts/configure.sh
+++ ecSource/scripts/configure.sh
@@ -8,62 +8,62 @@ elif [ $result == "EmissionControl2" ]; then
   cd ecSource/
 fi
 
+git submodule update --init --recursive
+
+if [ $(uname -s) == "Linux" ]; then
+  mkdir -p ./bin
+  cd ./bin
+  mkdir -p ./Resources
+  cp -r ../../externalResources/Fonts ./Resources
+  cp -r ../../externalResources/samples ./Resources
+  mkdir -p Resources/config_scripts
+  cp -p ../../externalResources/config_scripts/configAbsoDirectories-linux.sh ./Resources/config_scripts/
+  cd ..
+fi
+
+# Build LIBSAMPLERATE if it doesnt exist../external/libsamplerate/build
+if [ ! -d "./external/libsamplerate/build" ]; then
+  cmake -E make_directory external/libsamplerate/build
+  cd external/libsamplerate/build
+  cmake ..
+  make
+  cd ../../../
+fi
+
+# Build nativefiledialog if it doesnt exist../external/libsamplerate/build
+# if [ ! -d "./external/nativefiledialog/build/lib" ]; then
+#   cd external/nativefiledialog/build/
+#   if [ $(uname -s) == "Linux" ]; then
+#     cd gmake_linux
+#     make config=release_x64
+#   elif [ $(uname -s) == "Darwin" ]; then
+#     cd gmake_macosx
+#     make config=release_x64
+#   fi
+#   cd ../../../../
+# fi
+
+mkdir -p build
+cd build
+mkdir -p release
+cd release
+if [ $(uname -s) == "Linux" ]; then
+  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
+fi
+
+if [ $(uname -s) == "Darwin" ]; then
+  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
+fi
+
+cd ../../
+if [ ! -d "./external/nativefiledialog/build/lib" ]; then
+  cd external/nativefiledialog/build/
   if [ $(uname -s) == "Linux" ]; then
-    mkdir -p ./bin
-    cd ./bin
-    mkdir -p ./Resources
-    cp -r ../../externalResources/Fonts ./Resources
-    cp -r ../../externalResources/samples ./Resources
-    mkdir -p Resources/config_scripts
-    cp -p ../../externalResources/config_scripts/configAbsoDirectories-linux.sh ./Resources/config_scripts/
-    cd ..
+    cd gmake_linux
+    make config=release_x64
+  elif [ $(uname -s) == "Darwin" ]; then
+    cd gmake_macosx
+    make config=release_x64
   fi
-
-  # Build LIBSAMPLERATE if it doesnt exist../external/libsamplerate/build
-  if [ ! -d "./external/libsamplerate/build" ]; then
-    cmake -E make_directory external/libsamplerate/build
-    cd external/libsamplerate/build
-    cmake ..
-    make
-    cd ../../../
-  fi
-
-  # Build nativefiledialog if it doesnt exist../external/libsamplerate/build
-  # if [ ! -d "./external/nativefiledialog/build/lib" ]; then
-  #   cd external/nativefiledialog/build/
-  #   if [ $(uname -s) == "Linux" ]; then
-  #     cd gmake_linux
-  #     make config=release_x64
-  #   elif [ $(uname -s) == "Darwin" ]; then
-  #     cd gmake_macosx
-  #     make config=release_x64
-  #   fi
-  #   cd ../../../../
-  # fi
-
-  mkdir -p build
-  cd build
-  mkdir -p release
-  cd release
-  if [ $(uname -s) == "Linux" ]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
-  fi
-
-  if [ $(uname -s) == "Darwin" ]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
-  fi
-
-  cd ../../
-  if [ ! -d "./external/nativefiledialog/build/lib" ]; then
-    cd external/nativefiledialog/build/
-    if [ $(uname -s) == "Linux" ]; then
-      cd gmake_linux
-      make config=release_x64
-    elif [ $(uname -s) == "Darwin" ]; then
-      cd gmake_macosx
-      make config=release_x64
-    fi
-    cd ../../../../
-  fi
-
-
+  cd ../../../../
+fi
