78c509310051a5fcaa736980946c7cd3d05b312b
Rodney DuPlessis <rodney@rodneyduplessis.com>
2020-10-03 14:23:11 -0700

    added line to use make for nativefiledialog (commented out xcode line)

--- ecSource/scripts/configure.sh
+++ ecSource/scripts/configure.sh
@@ -10,8 +10,8 @@ fi
 
 # Make sure submodules exist.
 (
-cd ..
-git submodule update --init --recursive
+  cd ..
+  git submodule update --init --recursive
 )
 
 rm -f external/al_ext/assets3d/CMakeLists.txt
@@ -20,64 +20,65 @@ rm -f external/al_ext/spatialaudio/CMakeLists.txt
 rm -f external/al_ext/statedistribution/CMakeLists.txt
 
 (
-if [ $(uname -s) == "Linux" ]; then
-  mkdir -p ./bin
-  cd ./bin
-  mkdir -p ./Resources
-  cp -r ../../externalResources/Fonts ./Resources
-  cp -r ../../externalResources/samples ./Resources
-fi
+  if [ $(uname -s) == "Linux" ]; then
+    mkdir -p ./bin
+    cd ./bin
+    mkdir -p ./Resources
+    cp -r ../../externalResources/Fonts ./Resources
+    cp -r ../../externalResources/samples ./Resources
+  fi
 )
 
 # Build LIBSAMPLERATE if it doesnt exist../external/libsamplerate/build
 (
-if [ ! -d "./external/libsamplerate/build" ]; then
-  cmake -E make_directory external/libsamplerate/build
-  cd external/libsamplerate/build
-  cmake ..
-  make
-fi
+  if [ ! -d "./external/libsamplerate/build" ]; then
+    cmake -E make_directory external/libsamplerate/build
+    cd external/libsamplerate/build
+    cmake ..
+    make
+  fi
 )
 
 # Build nativefiledialog if it doesnt exist../external/libsamplerate/build
 (
-if [ ! -d "./external/nativefiledialog/build/lib" ]; then
-  cd external/nativefiledialog/build/
-  if [ $(uname -s) == "Linux" ]; then
-    cd gmake_linux
-    make config=release_x64
-  elif [ $(uname -s) == "Darwin" ]; then # note: can't get make file to work, relies on xcode bleh
-    cd xcode4
-    xcodebuild -scheme nfd build -project nfd.xcodeproj/ -configuration Release CFLAGS=-mmacosx-version-min=10.10 CXXFLAGS=-mmacosx-version-min=10.10
+  if [ ! -d "./external/nativefiledialog/build/lib" ]; then
+    cd external/nativefiledialog/build/
+    if [ $(uname -s) == "Linux" ]; then
+      cd gmake_linux
+      make config=release_x64
+    elif [ $(uname -s) == "Darwin" ]; then # note: can't get make file to work, relies on xcode bleh
+      cd gmake_macosx
+      make config=release_x64
+      # cd xcode4
+      # xcodebuild -scheme nfd build -project nfd.xcodeproj/ -configuration Release CFLAGS=-mmacosx-version-min=10.10 CXXFLAGS=-mmacosx-version-min=10.10
+    fi
   fi
-fi
-)
-
-
-(
-mkdir -p build
-cd build
-mkdir -p release
-cd release
-if [ $(uname -s) == "Linux" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
-fi
-
-if [ $(uname -s) == "Darwin" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
-fi
 )
 
 (
-mkdir -p build
-cd build
-mkdir -p debug
-cd debug
-if [ $(uname -s) == "Linux" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
-fi
+  mkdir -p build
+  cd build
+  mkdir -p release
+  cd release
+  if [ $(uname -s) == "Linux" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
+  fi
 
-if [ $(uname -s) == "Darwin" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
-fi
+  if [ $(uname -s) == "Darwin" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
+  fi
+)
+
+(
+  mkdir -p build
+  cd build
+  mkdir -p debug
+  cd debug
+  if [ $(uname -s) == "Linux" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
+  fi
+
+  if [ $(uname -s) == "Darwin" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
+  fi
 )
