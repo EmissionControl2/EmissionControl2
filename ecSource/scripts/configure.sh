9a446086e645eb865ff5c14175e6a6d31ad5ab78
Jack Kilgore <kilgorej99@gmail.com>
2021-03-21 13:36:33 -0700

    finished morph time tweaking

--- ecSource/scripts/configure.sh
+++ ecSource/scripts/configure.sh
@@ -51,10 +51,10 @@ rm -f external/al_ext/statedistribution/CMakeLists.txt
       cd gmake_linux
       make config=release_x64
     elif [[ "$OSTYPE" == "darwin"* ]]; then # note: can't get make file to work, relies on xcode bleh
-      # cd gmake_macosx
-      # make config=release_x64
-      cd xcode4
-      xcodebuild -scheme nfd build -project nfd.xcodeproj/ -configuration Release CFLAGS=-mmacosx-version-min=10.10 CXXFLAGS=-mmacosx-version-min=10.10
+      cd gmake_macosx
+      make config=release_x64
+      # cd xcode4
+      # xcodebuild -scheme nfd build -project nfd.xcodeproj/ -configuration Release CFLAGS=-mmacosx-version-min=10.10 CXXFLAGS=-mmacosx-version-min=10.10
     elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
       echo "BUILD NFD WINDOWS TODO"
     fi
